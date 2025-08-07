26e8466ec522b04afdec3e97252ccc8b428204d0
Takanori Ugai <ugai@fujitsu.com>
2025-08-07 12:25:17 +0900

    Update build.gradle.kts

--- Task1/Task1/build.gradle.kts
+++ Task1/Task1/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
     java
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.9"
     jacoco
     id("org.jetbrains.dokka") version "2.0.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
